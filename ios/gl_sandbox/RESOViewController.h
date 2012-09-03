be7ff9b3af1373eb19c730ed61960353fe741e5b
Daniel Stepp <willstepp@gmail.com>
2012-09-03 10:21:16 -0400

    added ios sandbox project

---
+++ ios/gl_sandbox/RESOViewController.h
@@ -0,0 +1,13 @@
+//
+//  RESOViewController.h
+//  gl_sandbox
+//
+//  Created by Daniel Stepp on 9/1/12.
+//  Copyright (c) 2012 Monomyth Software. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface RESOViewController : GLKViewController
+
+@end
